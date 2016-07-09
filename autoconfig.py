2b2cdc7f49724356af768456f851b9ac96ece8b9
Gogo Lejzr <gogo.lejzr@gmail.com>
2016-07-09 14:23:01 +0200

    finished i3 configuration, added compton presets, now script configures i3, i3blocks, xresources and compton

--- autoconfig.py
+++ autoconfig.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import pytoml as toml
 import sys
 import subprocess
@@ -6,13 +8,21 @@ import re
 config = []
 theme = []
 
+#sed wrapper
+def sedw(placeholder, new_string, filename):
+    return subprocess.call(['sed', '-i', 's@##{}##@{}@g'.format(placeholder,new_string), filename])
+
+
 COLORS = ['black', 'darkGrey', 'darkRed', 'red', 'darkGreen', 'green', 
         'darkYellow', 'yellow', 'darkBlue', 'blue', 'darkMagenta', 'magenta', 
         'cyan', 'darkCyan', 'lightGrey', 'white', 'foreground', 'background']
 
 I3_WINDOWS = ['focused', 'inactive', 'unfocused', 'urgent', 'foreground']
 
-TEMPLATES = ['i3config', 'xresources']
+TEMPLATES = ['i3config', 'xresources', 'i3blocks']
+
+#shadow presets for compton
+SHADOWS = ['mild', 'thick', 'none']
 
 with open('config.toml', 'rb') as config_file:
     config = toml.load(config_file)
@@ -48,7 +58,8 @@ for template in TEMPLATES:
     for color in COLORS:
         color_val = theme['colors'][color]
         #print("{} : {}".format(color, color_val)) 
-        subprocess.call(['sed', '-i', 's/##{}##/{}/'.format(color, color_val),template_name])
+        #subprocess.call(['sed', '-i', 's/##{}##/{}/g'.format(color, color_val), template_name])
+        sedw(color, color_val, template_name)
 
 #additional i3 config block
 #i3 windows conf
@@ -62,19 +73,59 @@ for window in I3_WINDOWS:
         color_val = theme['colors'][color]
     
     print("{} {} {}".format(window, color, color_val))
-    subprocess.call(['sed', '-i', 's/##i3_{}##/{}'.format(window, color_val)], 'i3config.temp')
+    #subprocess.call(['sed', '-i', 's/##i3_{}##/{}/g'.format(window, color_val), 'i3config.temp'])
+    sedw('i3_' + window, color_val, 'i3config.temp')
 
 #gaps conf
-border = config['i3']['border']
-gaps_inner = config['i3']['gaps-inner']
-gaps_outer = config['i3']['gaps-outer']
+border = theme['i3']['border']
+gaps_inner = theme['i3']['gaps-inner']
+gaps_outer = theme['i3']['gaps-outer']
 
 if gaps_inner == 0 and gaps_outer == 0:
     
-    if border == 0:
+    #remove i3gaps calls
+    subprocess.call(['sed', '-i', 's/gaps_inner/##/g', 'i3config.temp'])
+    subprocess.call(['sed', '-i', 's/gaps_outer/##/g', 'i3config.temp'])
+    
+    if border != 0:
+        #subprocess.call(['sed', '-i', 's/##border##/{}'.format(border), 'i3config.temp'])
+        sedw('border', border, 'i3config.temp')
+
+else:
+    sedw('gaps_inner', gaps_inner, 'i3config.temp')
+    sedw('gaps_outer', gaps_outer, 'i3config.temp')
+    sedw('border', border, 'i3config.temp')
+
+#fonts
+for template in TEMPLATES:
+    template_name = '{}.temp'.format(template)
+    sedw('font_family', theme['other']['font'], template_name)
+    sedw('font_size', theme['other']['font-size'], template_name)
+
+#wallpaper
+sedw('wallpaper', theme['other']['wallpaper'], 'i3config.temp')
 
 #coping temp files to their targets
 for template in TEMPLATES:
+    template_name = '{}.temp'.format(template)
     target_path = config['paths'][template]
-    #cp neco
-    subprocess.call(['rm', '-f', '{}.temp'.format(template)])
+    subprocess.call(['cp', template_name, target_path])
+    subprocess.call(['rm', '-f', template_name])
+
+#copying compton config
+shadows = theme['other']['shadows']
+if shadows not in SHADOWS:
+    print('{} not in compton shadows presets, defaults to none'.format(shadows))
+    shadows = 'none'
+
+compton_preset = 'compton_configs/compton.conf.{}'.format(shadows)
+#compton does not use any template so copying is done here
+subprocess.call(['cp', compton_preset, config['paths']['compton']])
+
+#reload settings
+subprocess.call(['xrdb', '-load', config['paths']['xresources']])
+subprocess.call(['i3-msg', 'restart'])
+subprocess.call(['pkill', 'compton'])
+subprocess.call(['compton', '-b'])
+subprocess.call(['feh', '--bg-scale', theme['other']['wallpaper']])
+
