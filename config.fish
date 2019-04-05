b370c016a92617218f58d06bf49512acbe142fdb
ggljzr <gogo.lejzr@gmail.com>
2019-04-05 22:33:56 +0200

    added fish config file

---
+++ config.fish
@@ -0,0 +1,24 @@
+rvm default
+set -gx PATH $PATH /opt/etcher-cli
+
+set fish_color_autosuggestion magenta
+set fish_color_command normal
+set fish_color_comment brgray
+set fish_color_cwd brgreen
+set fish_color_cwd_root brred
+set fish_color_end brgreen
+set fish_color_error brred
+set fish_color_escape brcyan
+set fish_color_history_current brcyan
+set fish_color_host \x2do\x1ecyan
+set fish_color_match brcyan
+set fish_color_normal normal
+set fish_color_operator brcyan
+set fish_color_param bryellow
+set fish_color_quote brgreen
+set fish_color_redirection brcyan
+set fish_color_search_match \x2d\x2dbackground\x3dred
+set fish_color_selection \x2d\x2dbackground\x3dred
+set fish_color_status brred
+set fish_color_user \x2do\x1egreen
+set fish_color_valid_path \x2d\x2dunderline
